79a911af135bca4f2e62c3b41756be95ff47cc9f
Vivian Gichure <viviangichure@gmail.com>
2025-06-09 13:08:08 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function superbowlWin(record) {
+  const winningRecord = record.find(game => game.result === "W");
+  return winningRecord ? winningRecord.year : undefined;
+}
